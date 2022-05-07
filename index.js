4a2448df9025596c90bd986bdcc4d548ad73c976
Jonathan Pool <pool@stulta.com>
2022-05-07 11:33:20 -0400

    Finished implementing batching

--- index.js
+++ index.js
@@ -5,146 +5,85 @@
 
 // ########## IMPORTS
 
-// Module to keep secrets local.
-require('dotenv').config({override: true});
-const {ID, AUTHCODE, ENVIRONMENT, INTERVAL} = process.env;
-const protocol = process.env[`${ENVIRONMENT}PROTOCOL`];
-const hostname = process.env[`${ENVIRONMENT}HOSTNAME`];
-const port = process.env[`${ENVIRONMENT}PORT`];
-// Module to make HTTP(S) requests.
-const client = require(protocol);
 // Module to perform tests.
 const {handleRequest} = require('testaro');
-
-// ########## VARIABLES
-let working = false;
+const fs = require('fs').promises;
 
 // ########## FUNCTIONS
 
-// Sends a request to the Aorta server and returns the response data.
-const makeAortaRequest = async (what, specs = {}) => {
-  const content = {
-    id: ID,
-    authCode: AUTHCODE,
-    what
-  };
-  Object.assign(content, specs);
-  const contentJSON = JSON.stringify(content);
-  const options = {
-    hostname,
-    port,
-    path: '/aorta/api',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(contentJSON)
-    }
-  };
-  const responseData = await new Promise(resolve => {
-    const request = client.request(options, response => {
-      const chunks = [];
-      response.on('data', chunk => {
-        chunks.push(chunk);
-      });
-      // When the response is complete:
-      response.on('end', () => {
-        // Return its data.
-        resolve(JSON.parse(chunks.join('')));
-      });
+// Converts a script to a batch-based array of scripts.
+const batchify = (script, batch, timeStamp) => {
+  const {hosts} = batch;
+  const specs = hosts.map(host => {
+    const newScript = Object.assign({}, script);
+    newScript.acts.forEach(act => {
+      if (act.type === 'url') {
+        act.which = host.which;
+        act.what = host.what;
+      }
     });
-    request.end(contentJSON);
+    const spec = {
+      id: `${timeStamp}-${host.id}`,
+      script: newScript
+    };
+    return spec;
   });
-  return responseData;
+  return specs;
 };
-// Waits.
-const wait = ms => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('');
-    }, ms)
-  });
-};
-// Asks Aorta to assign an order to this tester.
-const claimOrder = async () => {
-  // Get the orders.
-  const orders = await makeAortaRequest('seeOrders');
-  // If there are any:
-  if (Array.isArray(orders)) {
-    if (orders.length) {
-      // Ask Aorta to make the first order a job assigned to this tester.
-      const orderName = orders[0].id;
-      return {
-        response: await makeAortaRequest('claimOrder', {
-          orderName,
-          testerName: ID
-        })
-      };
-    }
-    else {
-      return 'noOrders';
-    }
-  }
-  else {
-    return orders;
-  }
-};
-// Performs the first Aorta job assigned to this tester, submits a report, and returns the status.
-const doJob = async () => {
-  if (working) {
-    return 'skipped';
-  }
-  else {
-    working = true;
-    // Get the jobs.
-    const jobs = await makeAortaRequest('seeJobs');
-    // If there are any:
-    if (Array.isArray(jobs)) {
-      if (jobs.length) {
-        // Perform the first one.
-        const job = jobs[0];
-        await handleRequest(job);
-        // Submit the report to Aorta.
-        const jobResult = await makeAortaRequest('createReport', {report: job});
-        working = false;
-        return JSON.stringify(jobResult, null, 2);
-      }
-      else {
-        working = false;
-        return 'noJobs';
-      }
-    }
-    else {
-      working = false;
-      return jobs;
-    }
-  }
-};
-// Repeatedly claims orders, performs jobs, and submits reports.
-const cycle = async () => {
-  const interval = Number.parseInt(INTERVAL);
-  while (true) {
-    await wait(interval);
-    const jobResult = await doJob();
-    if (jobResult === 'noJobs') {
-      console.log('noJobs');
-      const orderResult = await claimOrder();
-      console.log(orderResult === 'noOrders' ? 'noOrders' : JSON.stringify(orderResult, null, 2));
-    }
-    else {
-      console.log(jobResult);
-    }
+// Calls Testaro.
+const callTestaro = async (script, startTime, id) => {
+  const options = {
+    log: [],
+    report: {
+      id,
+      startTime
+    },
+    script
   };
+  await handleRequest(options);
+  const optionsJSON = JSON.stringify(options, null, 2);
+  await fs.writeFile(`results/${options.report.id}.json`, optionsJSON);
+};
+// Runs a job.
+const run = async () => {
+  const scriptName = process.argv[2];
+  const batchName = process.argv[3];
+  if (scriptName) {
+    try {
+      const scriptJSON = await fs.readFile(`scripts/${scriptName}.json`, 'utf8');
+      const script = JSON.parse(scriptJSON);
+      let batch = null;
+      // Identify the start time and a timestamp.
+      const startTime = (new Date()).toISOString().slice(0, 19);
+      const timeStamp = Math.floor((Date.now() - Date.UTC(2022, 1)) / 2000).toString(36);
+      // If there is a batch:
+      if (batchFileName) {
+        // Convert the script to a batch-based set of scripts.
+        const batchJSON = await fs.readFile(`batches/${batchName}.json`, 'utf8');
+        batch = JSON.parse(batchJSON);
+        const specs = batchify(script, batch, timeStamp);
+        // For each script:
+        while (specs.length) {
+          const spec = specs.shift();
+          // Call Testaro on it and save the result with a host-suffixed ID.
+          await callTestaro(spec.script, startTime, spec.id);
+        }
+      }
+      // Otherwise, i.e. if there is no batch:
+      else {
+        // Call Testaro on the script and save the result with a timestamp ID.
+        await callTestaro(script, startTime, timeStamp);
+      }
+    }
+    catch(error) {
+      console.log(`ERROR: ${error.message}\n${error.stack}`);
+    }
+  }
+  else {
+    console.log('ERROR: no script specified');
+  }
 };
 
 // ########## OPERATION
 
-try {
-  cycle();
-}
-catch(error) {
-  console.log(`ERROR: ${error.message}`);
-  const interval = Number.parseInt(INTERVAL);
-  setTimeout(() => {
-    cycle();
-  }, interval)
-}
+run();

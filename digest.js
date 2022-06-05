98d4eb84f9726f409bb22b1c16d07b7885fd6d67
Jonathan Pool <pool@stulta.com>
2022-06-05 09:10:55 -0400

    Implemented batch digesting

--- digest.js
+++ digest.js
@@ -14,7 +14,7 @@ const fs = require('fs').promises;
 
 const reportDirScored = process.env.REPORTDIR_SCORED || 'reports/scored';
 const reportDirDigested = process.env.REPORTDIR_DIGESTED || 'reports/digested';
-const reportID = process.argv[2];
+const reportIDStart = process.argv[2];
 const digesterID = process.argv[3];
 
 // ########## FUNCTIONS
@@ -24,15 +24,23 @@ const replaceHolders = (content, query) => content
 .replace(/__([a-zA-Z]+)__/g, (ph, qp) => query[qp]);
 // Creates a digest.
 const digest = async () => {
-  const reportJSON = await fs.readFile(`${__dirname}/${reportDirScored}/${reportID}.json`, 'utf8');
-  const report = JSON.parse(reportJSON);
+  const reportDirScoredAbs = `${__dirname}/${reportDirScored}`;
+  const allReportFileNames = await fs.readdir(reportDirScoredAbs);
+  const sourceReportFileNames = allReportFileNames
+  .filter(fileName => fileName.startsWith(reportIDStart) && fileName.endsWith('.json'));
   const {makeQuery} = require(`${__dirname}/procs/digest/${digesterID}/index.js`);
-  const query = {};
-  makeQuery(report, query);
-  const template = await fs.readFile(`${__dirname}/procs/digest/${digesterID}/index.html`, 'utf8');
-  const digest = replaceHolders(template, query);
-  await fs.writeFile(`${__dirname}/${reportDirDigested}/${reportID}.html`, digest);
-  console.log(`Report ${reportID} digested and saved`);
+  for (const fileName of sourceReportFileNames) {
+    const reportJSON = await fs.readFile(`${reportDirScoredAbs}/${fileName}`, 'utf8');
+    const report = JSON.parse(reportJSON);
+    const query = {};
+    makeQuery(report, query);
+    const template = await fs
+    .readFile(`${__dirname}/procs/digest/${digesterID}/index.html`, 'utf8');
+    const digest = replaceHolders(template, query);
+    const fileNameBase = fileName.slice(0, -5);
+    await fs.writeFile(`${__dirname}/${reportDirDigested}/${fileNameBase}.html`, digest);
+    console.log(`Report ${fileNameBase} digested and saved`);
+  };
 };
 
 // ########## OPERATION

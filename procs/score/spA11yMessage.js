6b948875e9047867b8777e5344b3f3045acae557
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-08-20 14:33:20 -0400

    Added load state to spA11yMessage and debugged

--- procs/score/spA11yMessage.js
+++ procs/score/spA11yMessage.js
@@ -9,7 +9,7 @@
 
   This proc computes a score that is intended to represent how accessibly a web page offers
     a user an opportunity to report an accessibility issue about that page. Scores can range
-    from 0 to 14.
+    from 0 to 16.
 */
 
 // CONSTANTS
@@ -52,35 +52,39 @@ exports.scorer = async report => {
   };
   const {score} = report;
   if (Array.isArray(acts)) {
-    // Act 1: page load.
+    // Act 1: page loads.
     if (acts[1].result.startsWith('http')) {
       score.page = 2;
-      // Act 2: click an accessibility link.
+      // Act 2: accessibility link exists and loads promptly.
       if (acts[2].result.move === 'clicked') {
         score.a11yLink = 2;
-        // Act 4: next page has an accessibility title.
-        const act4Result = acts[4].result;
-        if (act4Result && act4Result.toLowerCase().includes('accessibility')) {
+        const loadScore = ['incomplete', 'loaded', 'idle'].indexOf(acts[2].result.loadState);
+        if (loadScore > -1) {
+          score.a11yLink += loadScore;
+        }
+        // Act 3: next page has an accessibility title.
+        const act3Result = acts[3].result;
+        if (act3Result && act3Result.toLowerCase().includes('accessibility')) {
           score.title = 2;
-          // Act 5: page has exactly 1 h1 heading.
-          const act5Result = acts[5].result;
-          if (act5Result && act5Result.total === 1) {
+          // Act 4: page has exactly 1 h1 heading.
+          const act4Result = acts[4].result;
+          if (act4Result && act4Result.total === 1) {
             score.heading = 1;
-            // Act 5: h1 is an accessibility heading.
-            if (act5Result.items[0].textContent.toLowerCase().includes('accessibility')) {
+            // Act 4: h1 is an accessibility heading.
+            if (act4Result.items[0].textContent.toLowerCase().includes('accessibility')) {
               score.heading += 1;
             }
           }
         }
+        // Act 5: page has accessibility email and telephone links.
+        const act5Result = acts[5].result;
+        if (act5Result.total) {
+          contactScorer(act5Result, score, 'mailLink');
+        }
         // Act 6: page has accessibility email and telephone links.
         const act6Result = acts[6].result;
         if (act6Result.total) {
-          contactScorer(act6Result, score, 'mailLink');
-        }
-        // Act 7: page has accessibility email and telephone links.
-        const act7Result = acts[7].result;
-        if (act7Result.total) {
-          contactScorer(act7Result, score, 'telLink');
+          contactScorer(act6Result, score, 'telLink');
         }
       }
     }

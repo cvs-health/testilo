e15cda530cb453c20fc2c0428b16e223d02b0e65
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-08-23 11:22:40 -0400

    Revised spA11yMessage

--- procs/score/spA11yMessage.js
+++ procs/score/spA11yMessage.js
@@ -20,17 +20,18 @@ const scoreProcID = 'a11ymessage';
 // FUNCTIONS
 
 // Scores the contact links of a type.
-const contactScorer = (result, score, type) => {
+const contactScorer = (result, score, linkProp, linkNameProp) => {
   const links = result.items;
   if (links.some(
     link => link.textContent.toLowerCase().includes('accessibility')
   )) {
-    score[type] -= 3;
+    score[linkProp] = 2
+    score[linkNameProp] = 1;
   }
   else if (links.some(
     link => link.parentTextContent.toLowerCase().includes('accessibility')
   )) {
-    score[type] -= 2;
+    score[linkProp] = 2;
   }
 };
 // Scores a report.
@@ -38,67 +39,69 @@ exports.scorer = async report => {
   const {acts} = report;
   report.scoreProcID = scoreProcID;
   report.score = {
-    page: 3,
-    a11yLink: 4,
-    title: 3,
-    heading: 3,
-    mailLink: 3,
-    telLink: 3
+    pageLoad: 0,
+    pageFast: 0,
+    a11yLink: 0,
+    a11yLinkWork: 0,
+    a11yLinkFast: 0,
+    a11yPageTitle: 0,
+    a11yTitleGood: 0,
+    a11yPageH1: 0,
+    a11yH1Good: 0,
+    mailLink: 0,
+    mailLinkName: 0,
+    telLink: 0,
+    telLinkName: 0
   };
   const {score} = report;
   if (Array.isArray(acts)) {
-    // Act 1: page loads.
+    // Act 1: page load.
     if (acts[1].result.startsWith('http')) {
-      score.page -= 2;
+      score.pageLoad = 2;
       if (acts[1].endTime - acts[1].startTime < 2500) {
-        score.page -= 1;
+        score.pageFast = 1;
       }
-      // Act 2: accessibility link exists and loads promptly.
+      // Act 2: accessibility link.
       const {result} = acts[2];
-        // If a link with text content including accessibility was found:
-        if (result.found) {
-        score.a11yLink -= 2;
+      // If a link with text content including accessibility was found:
+      if (result.found) {
+        score.a11yLink = 1;
         // If it was clickable and the resulting load finished:
         if (result.success) {
-          score.a11yLink -= 1;
+          score.a11yLinkWork = 2;
           // If the navigation and load took less than 1.5 seconds:
           if (acts[2].endTime - acts[2].startTime < 1500) {
-            score.a11yLink -= 1;
+            score.a11yLinkFast = 1;
           }
           // Act 3: next page has an accessibility title.
           let {result} = acts[3];
           if (result && result.success) {
-            score.title -= 1;
+            score.a11yPageTitle = 1;
             if (result.title.toLowerCase().includes('accessibility')) {
-              score.title -= 2;
+              score.a11yTitleGood = 2;
             }
           }
-          // Act 4: page has 1 h1 heading, and it is about accessibility.
+          // Act 4: page has an accessibility heading.
           result = acts[4].result;
           if (result && result.total === 1) {
-            score.heading -= 1;
+            score.a11yPageH1 = 1;
             if (result.items[0].textContent.toLowerCase().includes('accessibility')) {
-              score.heading -= 2;
+              score.a11yH1Good = 2;
             }
           }
           // Act 5: page has an accessibility email link.
           result = acts[5].result;
           if (result.total) {
-            contactScorer(result, score, 'mailLink');
+            contactScorer(result, score, 'mailLink', 'mailLinkName');
           }
           // Act 6: page has accessibility telephone link.
           result = acts[6].result;
           if (result.total) {
-            contactScorer(result, score, 'telLink');
+            contactScorer(result, score, 'telLink', 'telLinkName');
           }
         }
       }
     }
   }
-  score.total = score.page
-  + score.a11yLink
-  + score.title
-  + score.heading
-  + score.mailLink
-  + score.telLink;
+  score.total = Object.values(score).reduce((total, current) => total + current);
 };
